2ee493911b1d6f14fdbed37d6c064e042f39ddb6
bnnto <gbento555@gmail.com>
2025-10-13 18:19:54 -0300

    umas coisinhas

--- PatasNaRua/ong/migrations/0003_alter_pet_foto.py
+++ PatasNaRua/ong/migrations/0003_alter_pet_foto.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ong', '0002_pet_foto'),
+        ('ong', '0001_initial'),
     ]
 
     operations = [
