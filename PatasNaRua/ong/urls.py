9d928e2c2d44638f33adc226469d1c298899b6c1
joao0cb <joao.00000853774@unicap.br>
2025-10-17 14:46:22 -0300

    Att cadpet e infopet

--- PatasNaRua/ong/urls.py
+++ PatasNaRua/ong/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('cadastro-pet/', views.cadpet_page, name='cadpet_page'),
     path('api/cadpet/', views.cadpet_view, name='cadpet_api'),
     path('infopet-ong/', views.infopet_ong, name='infopet_ong'),
-    path('localpet-ong/', views.localpet_ong, name='localpet_ong')
+    path('localpet-ong/', views.localpet_ong, name='localpet_ong'),
+    path('pets/<int:pet_id>/remover/', views.remover_pet, name='remover_pet'),
 ]
 
 if settings.DEBUG:
